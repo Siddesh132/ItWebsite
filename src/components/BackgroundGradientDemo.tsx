4cfb72c2c4c36feabf4d4b03c05fb8cc89d7e8a1
siddu002 <51789609+siddu002@users.noreply.github.com>
2025-01-10 11:56:45 +0530

    changes updated

--- src/components/BackgroundGradientDemo.tsx
+++ src/components/BackgroundGradientDemo.tsx
@@ -14,7 +14,7 @@ export function BackgroundGradientDemo() {
   return (
     <div className="flex flex-wrap md:flex-row justify-center items-center p-8 bg-neutral-950  gap-8">
       {/* Box 1 */}
-      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 max-w-sm bg-white dark:bg-zinc-900">
+      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 max-w-sm bg-black dark:bg-zinc-900">
         <Image
           src={imgone}
           alt="jordans"
@@ -22,7 +22,7 @@ export function BackgroundGradientDemo() {
           width="400"
           className="object-contain"
         />
-        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        <p className="text-base sm:text-xl text-white mt-4 mb-2 dark:text-neutral-200">
           Enhancing Autonomous Vehicle Systems through High-Quality Data Annotation
         </p>
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
@@ -31,7 +31,7 @@ export function BackgroundGradientDemo() {
       </BackgroundGradient>
 
       {/* Box 2 */}
-      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 max-w-sm bg-white dark:bg-zinc-900">
+      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 max-w-sm bg-black dark:bg-zinc-900">
         <Image
           src={AgriImg}
           alt="jordans"
@@ -39,14 +39,14 @@ export function BackgroundGradientDemo() {
           width="400"
           className="object-contain"
         />
-        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        <p className="text-base sm:text-xl text-white mt-4 mb-2 dark:text-neutral-200">
           AI-Powered Agricultural Solutions with Annotated Visual Data
         </p>
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
           Learn how Prithvi Technologies supported an agricultural AI solution with accurate annotation of crops and soil data, enhancing model performance.
         </p>
       </BackgroundGradient>
-      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 max-w-sm bg-white dark:bg-zinc-900">
+      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 max-w-sm bg-black dark:bg-zinc-900">
         <Image
           src={PolygonImg}
           alt="jordans"
@@ -54,14 +54,14 @@ export function BackgroundGradientDemo() {
           width="400"
           className="object-contain"
         />
-        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        <p className="text-base sm:text-xl text-white mt-4 mb-2 dark:text-neutral-200">
         Improving Medical Imaging AI with Polygon Annotation
         </p>
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
         Learn how Prithvi Technologies supported an agricultural AI solution with accurate annotation of crops and soil data, enhancing model performance.
         </p>
       </BackgroundGradient>
-      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 max-w-sm bg-white dark:bg-zinc-900">
+      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 max-w-sm bg-black dark:bg-zinc-900">
         <Image
           src={DataAna}
           alt="jordans"
@@ -69,7 +69,7 @@ export function BackgroundGradientDemo() {
           width="400"
           className="object-contain"
         />
-        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        <p className="text-base sm:text-xl text-white mt-4 mb-2 dark:text-neutral-200">
            5 Ways to Ensure Accurate Annotations for Your AI Projects
         </p>
            <ul className="text-sm text-neutral-600 dark:text-neutral-400">
@@ -81,7 +81,7 @@ export function BackgroundGradientDemo() {
            </ul>
          
       </BackgroundGradient>
-      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 max-w-sm bg-white dark:bg-zinc-900">
+      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 max-w-sm bg-black dark:bg-zinc-900">
         <Image
           src={datalabel}
           alt="jordans"
@@ -89,14 +89,14 @@ export function BackgroundGradientDemo() {
           width="400"
           className="object-contain"
         />
-        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        <p className="text-base sm:text-xl text-white mt-4 mb-2 dark:text-neutral-200">
           The Impact of AI on Data Labeling Automation 
         </p>
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
             AI-assisted data labeling enhances efficiency and accuracy but faces challenges with nuanced data and initial dataset requirements. A hybrid model, combining AI for bulk labeling and humans for quality control, is the future trend.
         </p>
       </BackgroundGradient>
-      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 max-w-sm bg-white dark:bg-zinc-900">
+      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 max-w-sm bg-black dark:bg-zinc-900">
         <Image
           src={medical}
           alt="jordans"
@@ -104,14 +104,14 @@ export function BackgroundGradientDemo() {
           width="400"
           className="object-contain"
         />
-        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        <p className="text-base sm:text-xl text-white mt-4 mb-2 dark:text-neutral-200">
             Key challenges in medical image annotation and how to overcome
         </p>
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
             Medical image annotation is complex, requiring expert knowledge and strict privacy measures. AI-assisted tools, expert involvement, and clear guidelines can improve accuracy and efficiency.
         </p>
       </BackgroundGradient>
-      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 max-w-sm bg-white dark:bg-zinc-900">
+      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 max-w-sm bg-black dark:bg-zinc-900">
         <Image
           src={imgone}
           alt="jordans"
@@ -119,14 +119,14 @@ export function BackgroundGradientDemo() {
           width="400"
           className="object-contain"
         />
-        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        <p className="text-base sm:text-xl text-white mt-4 mb-2 dark:text-neutral-200">
             The Rise of Data Annotation in Autonomous Driving
         </p>
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
              Explore how data labeling is revolutionizing the autonomous driving industry and the future trends we can expect to see.
         </p>
       </BackgroundGradient>
-      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 max-w-sm bg-white dark:bg-zinc-900">
+      <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 max-w-sm bg-black dark:bg-zinc-900">
         <Image
           src={health}
           alt="jordans"
@@ -134,7 +134,7 @@ export function BackgroundGradientDemo() {
           width="400"
           className="object-contain"
         />
-        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        <p className="text-base sm:text-xl text-white mt-4 mb-2 dark:text-neutral-200">
             AI in Healthcare – The Power of Annotated Medical Data
         </p>
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
